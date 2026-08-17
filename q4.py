4d590a26a051ef21f7b710cefab720564a136452
surbhisuman9142-stack <surbhisuman9142@gmail.com>
2026-08-17 23:58:30 +0530

    minimum number

---
+++ q4.py
@@ -0,0 +1,11 @@
+a = int(input("Enter first number :"))
+b = int(input("Enter second number :"))
+c = int(input("Enter third number :"))
+if a < b and a < c:
+    print ("minimum num  =", a)
+elif b < a and b < c:
+    print ("minimum num =" , b)
+else :
+    print ("minimum num =", c)
+
+    
