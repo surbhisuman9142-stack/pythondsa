631dd7f84ac5d4d8158d854c059220b727fe6f73
surbhisuman9142-stack <surbhisuman9142@gmail.com>
2026-08-18 22:25:11 +0530

    check prime

---
+++ q10.py
@@ -0,0 +1,11 @@
+def is_prime(n):
+    if n < 2:
+        return False
+    i = 2
+    while i * i <= n:
+        if n % i == 0:
+            return False
+        i += 1
+    return True
+n = int(input("Enter a number:"))
+print(is_prime(n))
