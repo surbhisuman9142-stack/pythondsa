7d1343ec55bc05ca6ed3ef1233dbcc20a6b32604
surbhisuman9142-stack <surbhisuman9142@gmail.com>
2026-08-19 16:27:17 +0530

    collatz step

---
+++ q17.py
@@ -0,0 +1,11 @@
+def collatz(n):
+    steps = 0
+    while n != 1:
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = 3*n+1
+        steps += 1
+    return steps
+n = int(input("Enter a number:"))
+print(collatz(n))
