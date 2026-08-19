43074fcfaae52ac6c99b9a53fe88bac0bca498ec
surbhisuman9142-stack <surbhisuman9142@gmail.com>
2026-08-19 15:43:52 +0530

    gcd of a&b

---
+++ q15.py
@@ -0,0 +1,9 @@
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("GCD =", gcd(a, b))
