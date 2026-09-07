d154b95dcdb79c6e1d975851dc89484ff82391a3
surbhisuman9142-stack <surbhisuman9142@gmail.com>
2026-09-07 16:05:54 +0530

    longest subarray sum<=k

---
+++ q54.py
@@ -0,0 +1,14 @@
+def logest_subarray(nums,k):
+    left = 0
+    window_sum = 0
+    max_length = 0
+    for right in range(len(nums)):
+        window_sum += nums[right]
+        while window_sum > k:
+            window_sum -= nums[left]
+            left += 1
+        max_length = max(max_length, right-left + 1)
+    return max_length
+nums = [2,1,5,1,3,2]
+k = 7
+print(logest_subarray(nums,k))
