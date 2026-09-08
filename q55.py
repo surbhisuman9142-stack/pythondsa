4d35900a6a71a3827d2cdedb9d5ca6093ad3607d
surbhisuman9142-stack <surbhisuman9142@gmail.com>
2026-09-08 14:00:41 +0530

    container with most water

---
+++ q55.py
@@ -0,0 +1,16 @@
+def maxArea(height):
+    left = 0
+    right = len(height)-1
+    max_water = 0
+    while left < right:
+        width = right - left
+        h = min(height[left],height[right])
+        area = width * h
+        max_water = max(max_water,area)
+        if height[left] < height[right]:
+            left += 1
+        else:
+            right -= 1
+    return max_water
+height = [1,8,6,2,5,4,8,3,7]
+print(maxArea(height))
