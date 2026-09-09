7c4ac6229b46ad44991f6f6bbf37d84e0d487be3
surbhisuman9142-stack <surbhisuman9142@gmail.com>
2026-09-09 16:03:14 +0530

    longest common prefix

---
+++ q68.py
@@ -0,0 +1,16 @@
+def longest_common_prefix(strs):
+    if not strs:
+        return ''
+
+    prefix = strs[0]
+
+    for s in strs[1:]:
+        while not s.startswith(prefix):
+            prefix = prefix[:-1]
+
+            if prefix == '':
+                return ''
+
+    return prefix
+s = ["flower", "flow", "flight"]
+print(longest_common_prefix(s))
